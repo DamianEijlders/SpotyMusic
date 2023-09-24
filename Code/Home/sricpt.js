fb1176232a8578231106857f01009e2f01f4da92
Damian <164895@student.horizoncollege.nl>
2023-09-24 14:59:57 +0200

    Working on Progressbar

--- Code/Home/sricpt.js
+++ Code/Home/sricpt.js
@@ -1,15 +1,28 @@
 const songcards = document.getElementById('songcards');
+const progressBar = document.getElementById('progressBar');
+const progressdata = document.getElementById('progressdata');
+
+// Function to show the progress bar
+function showProgressBar() {
+    progressBar.style.display = 'block';
+}
+
+// Function to hide the progress bar
+function hideProgressBar() {
+    progressBar.style.display = 'none';
+}
+
+// Add a click event listener to each button
 fetch('../songs.json')
     .then((response) => response.json())
     .then((data) => {
         const { songs } = data;
 
-        songs.forEach((song) => {
+        songs.forEach((song, index) => {
             const card = document.createElement('div');
             card.classList = 'text-center';
-            console.log(song);
             card.innerHTML = `
-        <div class="">
+        <button class="">
           <a
             href="#"
             class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
@@ -23,9 +36,23 @@ fetch('../songs.json')
             <p class="font-normal text-gray-700 dark:text-gray-400">
               Artist: ${song.artist} | Album: ${song.album} | Year: ${song.year}
             </p>
+            <p class="font-normal text-gray-700 dark:text-gray-400">
+              Index: ${index}
+            </p>
           </a>
-        </div>
+        </button>
       `;
+            const button = card.querySelector('button');
+            button.addEventListener('click', () => {
+                showProgressBar();
+                setTimeout(() => {
+                    progressdata.innerHTML = `Song: ${songs[index].title}`;
+                    console.log('Data for index', index, ':', songs[index]);
+
+                    // hideProgressBar();
+                }, 2000);
+            });
+
             songcards.appendChild(card);
         });
     });
