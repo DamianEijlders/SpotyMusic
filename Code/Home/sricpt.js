c07174af92a023a6cef16f6552e5a6d24d951571
Damian <164895@student.horizoncollege.nl>
2023-09-24 14:25:28 +0200

    Data is now being fetched correctly

--- Code/Home/sricpt.js
+++ Code/Home/sricpt.js
@@ -2,36 +2,29 @@ const songcards = document.getElementById('songcards');
 fetch('../songs.json')
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
+        const { songs } = data; // Access the array of songs
 
-        // Iterate through each array
-        for (const genre in data) {
-            if (data.hasOwnProperty(genre)) {
-                const songs = data[genre];
-
-                // Iterate through each song within the genre
-                songs.forEach((song) => {
-                    const card = document.createElement('div');
-                    card.classList.add('card');
-                    card.innerHTML = `
-                        <div class="">
-                            <a
-                                href="#"
-                                class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-                            >
-                                <h5
-                                    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-                                >
-                                    ${song.title}
-                                </h5>
-                                <p class="font-normal text-gray-700 dark:text-gray-400">
-                                    Genre: ${genre}
-                                </p>
-                            </a>
-                        </div>
-                    `;
-                    songcards.appendChild(card);
-                });
-            }
-        }
+        songs.forEach((song) => {
+            const card = document.createElement('div');
+            console.log(song);
+            card.innerHTML = `
+        <div class="">
+          <a
+            href="#"
+            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          >
+            <h5
+              class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              ${song.title}
+            </h5>
+            <img src="${song.img_file}" alt="Song Image" class="object-cover w-full h-48 mb-6 rounded-lg" />
+            <p class="font-normal text-gray-700 dark:text-gray-400">
+              Artist: ${song.artist} | Album: ${song.album} | Year: ${song.year}
+            </p>
+          </a>
+        </div>
+      `;
+            songcards.appendChild(card);
+        });
     });
